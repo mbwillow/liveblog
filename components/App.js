b96011e9ff8f4c0cfb50508fb9ee3b57cd4e8689
A Week of REST <aweekofrest@Siobhans-MacBook-Pro.local>
2016-09-08 09:05:50 +0100

    Day 2 fix :lambda

--- components/App.js
+++ components/App.js
@@ -3,8 +3,12 @@ import api from 'wordpress-rest-api-oauth-1'
 import Header from './Header'
 import PostsList from './PostsList'
 
-
 const SITE_URL = 'http://awor.local/'
+const API_KEY = 'JTFiOCfq1eGE'
+const API_SECRET = 'R5ZP9OEJpX0zPAYfSgCtCqisDH8eVdRlSb5W66eTDnjmlW03'
+const CALLBACK_URL = 'http://localhost:3000/'
+const BROKER_URL = 'http://awor.local/auth-broker/'
+
 
 export default class App extends React.Component {
 
@@ -13,12 +17,29 @@ export default class App extends React.Component {
 
 		this.state = {
 			posts: [],
-			isLoadingPosts: false
+			isLoadingPosts: false,
+			user: null
 		}
 
 		window.apiHandler = new api ({
-			url : SITE_URL
+			url: SITE_URL,
+			brokerURL: BROKER_URL,
+			brokerCredentials: {
+				client: {
+					public: API_KEY,
+					secret: API_SECRET
+				}
+			},
+			callbackURL: CALLBACK_URL
 		})
+
+		window.apiHandler.restoreCredentials()
+
+		if (window.apiHandler.hasCredentials() ){
+			this.onLoggedIn()
+		}else if (window.apiHandler.hasRequestToken() ){
+			this.onLogin()
+		}
 
 	}
 
@@ -32,20 +53,49 @@ export default class App extends React.Component {
 
 		this.setState({ isLoadingPosts: true })
 
-		let args = { _embed : true }
+		let args = { 
+			_embed : true ,
+			per_page: 100,
+			status: this.state.user ?  'any' : 'publish',
+			context: this.state.user ?  'edit' : 'view'
+
+		}
 
 		window.apiHandler.get( '/wp/V2/posts', args)
 			.then( posts => this.setState({posts, isLoadingPosts: false}) )
 	}
 
+	onLogin(){
+
+		window.apiHandler.authorize()
+		.then( () => this.onLoggedIn() )
+	}
+
+	onLogout(){
+		this.setState({user:null}, () => this.loadPosts() )
+		window.apiHandler.removeCredentials()
+	}
+
+	onLoggedIn() {
+		window.apiHandler.get('/wp/v2/users/me', {_envelope: true})
+			.then(data => data.body)
+			.then(user => this.setState({ user }))
+			.then( () => this.loadPosts() )
+			
+	}
+
 	render() {
 		return <div className = "app">
-			<Header/>			
+			<Header
+				user={this.state.user}
+				onLogin={() => this.onLogin()}
+				onLogout={() => this.onLogout()}
+			/>			
 			<div className="posts">
 				<PostsList 
 					isLoadingPosts={this.state.isLoadingPosts}
 					posts={this.state.posts} 
-					showFilter={true}/>
+					showFilter={!!this.state.user}/>
 			</div>
 		</div>		
 	}
--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
-export default class App extends React.Component {
+export default class Header extends React.Component {
 	render() {
 		return 	<header className="Header">
 					<img src="images/logo.png" />
+					<p className="actions">
+						{this.props.user ?
+							<button  onClick={() => this.props.onLogout()} className= "logout">Log Out</button>
+						:
+							<button onClick={() => this.props.onLogin()} className= "login">LogIn</button>
+						}
+					</p>
 				</header>
 	}
+}
+
+Header.propTypes = {
+	onLogin: React.PropTypes.func.isRequired,
+	onLogout: React.PropTypes.func.isRequired,
+	user: React.PropTypes.object
 }
